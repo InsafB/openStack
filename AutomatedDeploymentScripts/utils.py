ff0d0a63de74b9f6cbca7720e7874e6c9b149138
InsafB <boukrouh.insaf@gmail.com>
2016-12-23 19:57:39 +0000

    Update utils.py

--- AutomatedDeploymentScripts/utils.py
+++ AutomatedDeploymentScripts/utils.py
@@ -9,12 +9,20 @@ def print_values(val, type):
         for k, v in p.items():
             print("%s : %s" % (k, v))
         print('\n')
-
+        
+def print_values_ip(ip_list):
+    ip_dict_lisl = []
+    for ip in ip_list:
+        print("-"*35)
+        print("fixed_ip : %s" % ip.fixed_ip)
+        print("id : %s" % ip.id)
+        print("instance_id : %s" % ip.instance_id)
+        print("ip : %s" % ip.ip)
+        print("pool : %s" % ip.pool)
 
 def print_values_server(val, server_id, type):
     if type == 'ports':
         val_list = val['ports']
-
     if type == 'networks':
         val_list = val['networks']
     for p in val_list:
