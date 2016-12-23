036445a68d3ad1c8eb840663036c53ca0fb80efd
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2016-12-23 02:37:50 +0100

    Update i.py

--- TemplateServerM/templates/i.py
+++ TemplateServerM/templates/i.py
@@ -22,17 +22,15 @@ def index():
     # Verify if a session is already open
     if 'username' in session:
         username_session = escape(session['username']).capitalize()
-        '''
-        Save the user's name, and render the index template ( with the name of the user 
-                                                              -- done with flask code in index.html )
-        '''
+        # Save the user's name, and render the index template ( with the name of the user -- done with flask code in index.html )
         return render_template('index.html', session_user_name=username_session)
     return redirect(url_for('login'))
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/<int:id>', methods=['GET', 'POST'])
 def login():
     error = None
+    user_id = None
     if 'username' in session:
         return redirect(url_for('index'))
     if request.method == 'POST':
@@ -43,23 +41,24 @@ def login():
         if not cur.fetchone()[0]:
             error = "Invalid login"
         else:
-            cur.execute("SELECT password FROM users WHERE name = %s;", [username_form])
+            cur.execute("SELECT password,user_id FROM users WHERE name = %s;", [username_form])
             # Password is hashed -- compare the hashes
             if md5(password_form).hexdigest() == cur[1][0]:
                 session['username'] = request.form['username']
+                user_id = cur[1][1]
                 return redirect(url_for('index'))
             else:
                 error = "Invalid password"
             
-    return render_template('index.html', error=error)
+    return render_template('index.html', error=error, id=user_id)
 
 
-@app.route('/')
+@app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'just a simple secret key'
 
 if __name__ == '__main__':
     app.run(debug=True)
