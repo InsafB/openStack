5ef014026fba0cce2381659c4117e26635ba30c0
Ansari-Othmane <otmane.ansari@usmba.ac.ma>
2016-12-24 03:28:10 +0100

    Update credentials.py

--- AutomatedDeploymentScripts/credentials.py
+++ AutomatedDeploymentScripts/credentials.py
@@ -5,7 +5,19 @@ def get_credentials():
     d['auth_url'] = os.environ['OS_AUTH_URL']
     d['tenant_name'] = os.environ['OS_TENANT_NAME']
     return d
-    
+
+def get_nova_credentials_v2():
+    d = {}
+    d['version'] = '2'
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_PROJECT_ID']
+    d['project_name']= os.environ['OS_PROJECT_NAME']
+    d['domain_name']= os.environ['OS_USER_DOMAIN_NAME']
+    return d
+
+
 def get_nova_credentials():
     d = {}
     d['username'] = os.environ['OS_USERNAME']
