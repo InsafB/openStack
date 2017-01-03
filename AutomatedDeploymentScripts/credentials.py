ac45908ecc81c263c6c7366c3327c6dd8d76fb12
InsafB <boukrouh.insaf@gmail.com>
2017-01-03 22:33:00 +0000

    Update credentials.py

--- AutomatedDeploymentScripts/credentials.py
+++ AutomatedDeploymentScripts/credentials.py
@@ -7,6 +7,15 @@ def get_credentials():
     d['tenant_name'] = os.environ['OS_PROJECT_NAME']
     return d
 
+def get_nova_credentials():
+    d = {}
+    d['version'] = '2'
+    d['username'] = os.environ['OS_USERNAME']
+    d['api_key'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_id'] = os.environ['OS_PROJECT_NAME']
+    return d
+
 def get_nova_credentials_v2():
     d = {}
     d['version'] = '2'
@@ -25,14 +34,4 @@ def get_session_credentials():
     d['key'] = os.environ['OS_PASSWORD']
     d['authurl'] = os.environ['OS_AUTH_URL']
     d['os_options'] = {'user_domain_id': 'Default','project_domain_id': 'Default','project_name': os.environ['OS_PROJECT_NAME']}
-    return d
-
-
-def get_nova_credentials():
-    d = {}
-    d['version'] = '2'
-    d['username'] = os.environ['OS_USERNAME']
-    d['api_key'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['project_id'] = os.environ['OS_PROJECT_NAME']
     return d
