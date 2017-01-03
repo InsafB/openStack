0327fc073062a66cad8f03e416837643b5acb1f8
InsafB <boukrouh.insaf@gmail.com>
2017-01-03 17:01:54 +0000

    Update credentials.py

--- AutomatedDeploymentScripts/credentials.py
+++ AutomatedDeploymentScripts/credentials.py
@@ -1,8 +1,9 @@
 def get_credentials():
     d = {}
+    d['version'] = '2'
     d['username'] = os.environ['OS_USERNAME']
     d['password'] = os.environ['OS_PASSWORD']
-    d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['auth_url'] = 'http://10.11.50.26:5000/v2.0'
     d['tenant_name'] = os.environ['OS_PROJECT_NAME']
     return d
 
@@ -20,8 +21,9 @@ def get_nova_credentials_v2():
 
 def get_nova_credentials():
     d = {}
+    d['version'] = '2'
     d['username'] = os.environ['OS_USERNAME']
     d['api_key'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
-    d['project_name'] = os.environ['OS_PROJECT_NAME']
+    d['project_id'] = os.environ['OS_PROJECT_NAME']
     return d
