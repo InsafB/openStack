28a73529eec9716838678b177398803b6b5513c5
Ansari-Othmane <otmane.ansari@usmba.ac.ma>
2017-01-11 00:05:13 +0100

    Update scripts_installation.py

--- AutomatedDeploymentScripts/scripts_installation.py
+++ AutomatedDeploymentScripts/scripts_installation.py
@@ -1,6 +1,7 @@
 import os_client_config
 import os
 import paramiko
+import credentials
 from neutronclient.v2_0 import client
 from swiftclient.client import Connection, ClientException
 from credentials import *
@@ -10,8 +11,8 @@ DNS = {}
 
 
 def createNetwork(network_name):
-    credentials = get_credentials()
-    neutron = client.Client(**credentials)
+	credentials = get_credentials()
+	neutron = client.Client(**credentials)
 	try:	
 		body_create_network = {'network': {'name': network_name,'admin_state_up': True}}
 		network = neutron.create_network(body=body_create_network)
@@ -26,6 +27,7 @@ def createNetwork(network_name):
 	return network_id 
 
 def createRouter(router_name):
+	credentials = get_credentials()
 	neutron = client.Client(**credentials)
 	neutron.format = 'json'
 	request = {'router': {'name': router_name,'admin_state_up': True}}
@@ -35,50 +37,52 @@ def createRouter(router_name):
 	return router_id
 
 def createPort(port_name,router_id,network_id):
+	credentials = get_credentials()
+	neutron = client.Client(**credentials)
 	body_create_port = {'port': {'admin_state_up': True,'device_id': router_id,'name': port_name,'network_id': network_id}}
 	response = neutron.create_port(body=body_create_port)
 	print(response)
 	print("Add Port to Network: Execution Completed")
-    
+	
 
 def exec_commands(commands,server):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server)    
-    for cmd in commands:
-        print "executing command : ",cmd
-        client_stdin, client_stdout, client_stderr = client.exec_command(cmd);
-        exit_status = client_stdout.channel.recv_exit_status()
-        print "exit status for command '",cmd," is : ",exit_status
+	client = paramiko.SSHClient()
+	client.load_system_host_keys()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(server)    
+	for cmd in commands:
+		print "executing command : ",cmd
+		client_stdin, client_stdout, client_stderr = client.exec_command(cmd);
+		exit_status = client_stdout.channel.recv_exit_status()
+		print "exit status for command '",cmd," is : ",exit_status
 
 def appendHost(ip,ServerName,dest):
-    command = "echo '"+ip+"    "+ServerName+"' >> /etc/hosts"
-    commands = [command]
-    exec_commands(commands,dest)
+	command = "echo '"+ip+"    "+ServerName+"' >> /etc/hosts"
+	commands = [command]
+	exec_commands(commands,dest)
 
 
 def set_dns():
-    dests = list(DNS.keys())
+	dests = list(DNS.keys())
 
-    for dest in dests:
+	for dest in dests:
 
-        for keydsn, value in DNS.items():
+		for keydsn, value in DNS.items():
 
-            appendHost(keydsn,value,dest)
+			appendHost(keydsn,value,dest)
 
 def install_mysql(server):
-    commands=["sudo apt-get update","sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password othmane'","sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password othmane'","sudo apt-get -y install mysql-server"]
-    exec_commands(commands,server)
+	commands=["sudo apt-get update","sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password othmane'","sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password othmane'","sudo apt-get -y install mysql-server"]
+	exec_commands(commands,server)
 
 def getNovaClient():
-    ## Nova Client
-    credentials = get_nova_credentials_v2()
-    nova_client = os_client_config.make_client('compute',**credentials)
-    return nova_client
+	## Nova Client
+	credentials = get_nova_credentials_v2()
+	nova_client = os_client_config.make_client('compute',**credentials)
+	return nova_client
 
 def getSwiftConn():
-    return Connection(**get_session_credentials())
+	return Connection(**get_session_credentials())
 
 def createSwiftContainers(Containers):
 	conn = getSwiftConn()
@@ -99,62 +103,79 @@ def getPicture(pictureToGet,containerName):
 	return picture
 
 def createVM(name,network_id):
-    novaclient = getNovaClient()
-    ## Initiate VM parameters 
-    image = nova_client.images.find(name="ubuntu1404")
-    flavor = nova_client.flavors.find(name="m1.small")
-    net = nova_client.networks.find(id=network_id)
-    nics = [{'net-id': net.id}]
-    ## Create VM
-    ServerName = "Server"+name
-    instance = nova_client.servers.create(name=ServerName, image=image,flavor=flavor, key_name="key_mac", nics=nics)
-    #appendHost(instance.to_dict()['addresses']['private'][0]['addr'],ServerName)
-    DNS[instance.to_dict()['addresses']['private'][0]['addr']] = ServerName
-    return instance,ServerName
+	novaclient = getNovaClient()
+	## Initiate VM parameters 
+	image = nova_client.images.find(name="ubuntu1404")
+	flavor = nova_client.flavors.find(name="m1.small")
+	net = nova_client.networks.find(id=network_id)
+	nics = [{'net-id': net.id}]
+	## Create VM
+	ServerName = "Server"+name
+	instance = nova_client.servers.create(name=ServerName, image=image,flavor=flavor, key_name="key_mac", nics=nics)
+	appendHost(instance.to_dict()['addresses']['private'][0]['addr'],ServerName,"localhost")
+	DNS[instance.to_dict()['addresses']['private'][0]['addr']] = ServerName
+	return instance,ServerName
 
 def link_VM_FloatingIP(network_id,ServerName):
-    nova_client = getNovaClient()
-    ##Ask for a floating IP
-    #floating_ip = nova_client.floating_ips.create(nova_client.floating_ip_pools.list()[0].name)
-    #link with an existing ip ( already created)
-    floating_ip = nova_client.floating_ips.find(id=network_id)
-    instance = nova_client.servers.find(name=ServerName)
-    instance.add_floating_ip(floating_ip)
+	nova_client = getNovaClient()
+	##Ask for a floating IP
+	#floating_ip = nova_client.floating_ips.create(nova_client.floating_ip_pools.list()[0].name)
+	#link with an existing ip ( already created)
+	floating_ip = nova_client.floating_ips.find(id=network_id)
+	instance = nova_client.servers.find(name=ServerName)
+	instance.add_floating_ip(floating_ip)
 
 def createVM_Master(network_id):
-    instance , ServerName = createVM("Master")
-    link_VM_FloatingIP(network_id,ServerName)
+	instance , ServerName = createVM("Master",network_id)
+	link_VM_FloatingIP(network_id,ServerName)
 
 def createVM_I():
-    instance , ServerName =createVM("I")
-    install_mysql(ServerName)
+	instance , ServerName =createVM("I",network_id)
+	install_mysql(ServerName)
 
 def createVM_S():
-    instance , ServerName =createVM("S")
-    install_mysql(ServerName)
+	instance , ServerName =createVM("S",network_id)
+	install_mysql(ServerName)
 
 def createVM_B():
-    instance , ServerName = createVM("B")
+	instance , ServerName = createVM("B",network_id)
 
 def createVM_P():
-    instance , ServerName = createVM("P")
+	instance , ServerName = createVM("P",network_id)
 
 def createVM_W():
-    instance , ServerName = createVM("W")
-       
+	instance , ServerName = createVM("W",network_id)
+	   
 ## Main 
-network_id = createNetwork('private_network1')
-router_id = createRouter('router1')
-createPort('port1',router_id, network_id)
+print("Creation of network")
+network_id = createNetwork('private_network_project')
+print("Creation of router")
+router_id = createRouter('router_project')
+print("Creation of port")
+createPort('port_project',router_id, network_id)
 
+print("Creation of VMs")
+print("Creation of Master")
 createVM_Master(network_id)
+print("Creation of I")
 createVM_I()
+print("Creation of S")
 createVM_S()
+print("Creation of B")
 createVM_B()
+print("Creation of P")
 createVM_P()
+print("Creation of W")
 createVM_W()
 
+print("Set-up of DNS")
 set_dns()
 
+print("Creation of Containers")
 createSwiftContainers(['containerProfiles','containerPrices'])
+
+
+# set a list of sending commands
+#print("Sending the Master")
+
 
