3758c5c2e0cec9758a4492bc1b939fd2e73b2993
InsafB <boukrouh.insaf@gmail.com>
2017-01-10 18:07:47 +0100

    Update scripts_dependencies.sh

--- AutomatedDeploymentScripts/scripts_dependencies.sh
+++ AutomatedDeploymentScripts/scripts_dependencies.sh
@@ -4,11 +4,44 @@ export LC_ALL=C
 
 sudo apt-get update
 
-sudo apt -y install python3-pip
+if sudo apt -y install python-pip ; then
+    echo "python-pip installation succeeded"
+else
+    echo "python-pip installation failed"
+fi
 
-sudo apt-get -y install python-openstackclient
-sudo apt-get -y install python-swiftclient 
-sudo apt-get -y install python-neutronclient
+if sudo apt-get -y install python-openstackclient ; then
+    echo "openstackclient installation succeeded"
+else
+    echo "openstackclient installation failed"
+fi
 
-sudo pip3 install flask
-sudo apt-get -y install python-mysqldb
+if sudo apt-get -y install python-swiftclient ; then
+    echo "swiftclient installation succeeded"
+else
+    echo "swiftclient installation failed"
+fi
+
+if sudo apt-get -y install python-neutronclient ; then
+    echo "neutronclient installation succeeded"
+else
+    echo "neutronclient installation failed"
+fi
+
+if sudo apt-get -y install python-paramiko ; then
+    echo "paramiko installation succeeded"
+else
+    echo "paramiko installation failed"
+fi
+
+if sudo pip install flask ; then
+    echo "flask installation succeeded"
+else
+    echo "flask installation failed"
+fi
+
+if sudo apt-get -y install python-mysqldb ; then
+    echo "mysqldb installation succeeded"
+else
+    echo "mysqldb installation failed"
+fi
