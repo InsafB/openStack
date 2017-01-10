842c585da7ab0265c66e99d0daec0203c0df8b5c
Ansari-Othmane <otmane.ansari@usmba.ac.ma>
2017-01-10 22:56:29 +0100

    Create initial_dependencies.sh

---
+++ AutomatedDeploymentScripts/initial_dependencies.sh
@@ -0,0 +1,36 @@
+
+#!/bin/sh
+
+export LC_ALL=C
+
+sudo apt-get update
+
+if sudo apt -y install python3-pip ; then
+    echo "python3-pip installation succeeded"
+else
+    echo "python3-pip installation failed"
+fi
+
+if sudo apt-get -y install python-openstackclient ; then
+    echo "openstackclient installation succeeded"
+else
+    echo "openstackclient installation failed"
+fi
+
+if sudo apt-get -y install python-swiftclient ; then
+    echo "swiftclient installation succeeded"
+else
+    echo "swiftclient installation failed"
+fi
+
+if sudo apt-get -y install python-neutronclient ; then
+    echo "neutronclient installation succeeded"
+else
+    echo "neutronclient installation failed"
+fi
+
+if sudo apt-get -y install python-paramiko ; then
+    echo "paramiko installation succeeded"
+else
+    echo "paramiko installation failed"
+fi
