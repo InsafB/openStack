8752fb8431804437cc8d78abc42b7a84a4448143
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2017-01-14 04:45:27 +0100

    Update b.py

--- Services/b/b.py
+++ Services/b/b.py
@@ -5,6 +5,7 @@ import requests
 import base64
 import os
 import json
+import binascii
 from swiftclient.client import Connection, ClientException
 from credentials import *
 
@@ -12,36 +13,36 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET', 'POST'])
 def playLaunch():
-    
-    '''# POST with form-encoded data
-    response = requests.post(url, data=payload)'''
-
     user_id = request.args.get('user_id')
-    url = 'http://ServerW:5050/play/'+user_id
-    #url = 'http://0.0.0.0:8090/play/1'
-    
-    # GET with params in URL
-    response = requests.get(url)
-    #Receive data and create img
-    byte = json.loads(response.text)
-    image = bytes(byte['img'], 'ascii')
-    with open("imageToSave.png", "wb") as fh:
-        fh.write(base64.decodestring(image))
-
-    # Save image in swift
-    puchImageSwift("imageToSave.png",user_id+".png","containerB")
-
-    #delete tmp file
-    os.remove("imageToSave.png")
+    try:
+        url = 'http://ServerW:80/play/'+user_id
+        # GET with params in URL
+        response = requests.get(url)
+        #Receive data from W and create img
+        byte = json.loads(response.text)
+        image = bytes(byte['img'], 'ascii')
+        ###print("***Service B: image content before insert", image)
+        image_name = "images/"+str(user_id)+".jpg"
+        with open(image_name, "wb") as fh:
+            fh.write(base64.decodestring(image))
+        # Save image in swift
+        puchImageSwift(image_name,user_id+".jpg","containerB")
+        #delete tmp file
+        os.remove(image_name)
+        print("*********Service B: Image added**********")
+    except:
+        return "W_out"
 
     # Add user to status DB
-    url = 'https://ServerS:5050/save/'
-    payload = {'user_id': user_id}
-    # GET with params in URL
-    #response = requests.get(url, params=payload)
+    try:
+        #print("********Service B: Call Service S: Save play")
+        url = 'http://ServerS:80/save'
+        payload = {'user_id': user_id}
+        response = requests.get(url, params=payload)
+        return "UserAdded"
+    except requests.ConnectionError:
+        return "S_out"    
 
-    return "True"
-    
 @app.route("/test",methods=['GET', 'POST'])
 def gettest():
     print(" request catched ")
@@ -57,10 +58,8 @@ def puchImageSwift(pictureToPut,pictureNewName,containerName):
     conn.put_object(containerName, pictureNewName, file_data)
     print("Put Picture: Execution completed")   
 
-
-
 if __name__ == "__main__":
 
     #app.run(host = socket.gethostbyname(socket.gethostname()), port=80)
-    app.run(host = '10.0.1.7', port=80)
+    app.run(host = 'ServerB', port=80)
     
