7e3905d42c752d9adfa8e2a4c20e21e749c388b8
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2017-01-17 16:49:34 +0100

    Update p.py

--- Services/p/p.py
+++ Services/p/p.py
@@ -8,23 +8,23 @@ app = Flask(__name__)
 @app.route("/",methods=['GET', 'POST'])
 
 def getImage():
-    try:
-        user_id = request.args.get('user_id')
-        #image = "images/"+str(user_id)+".jpg"
-        image = str(user_id)+".jpg"
-        #print("*******Image:", image)
-        swiftimage = getImageSwift(image, "ContainerB")
-        return send_file(swiftimage, mimetype='image/jpg')
-    except FileNotFoundError:
-        return "NoFile"
+	try:
+		user_id = request.args.get('user_id')
+		#image = "images/"+str(user_id)+".jpg"
+		image = str(user_id)+".jpg"
+		#print("*******Image:", image)
+		swiftimage = getImageSwift(image, "ContainerB")
+		return send_file(swiftimage, mimetype='image/jpg')
+    	except FileNotFoundError:
+        	return "NoFile"
 
 def getSwiftConn():
-    return Connection(**get_session_credentials())
+	return Connection(**get_session_credentials())
 
-def getImageSwift(pictureToPut,pictureNewName,containerName):
-    conn = getSwiftConn()
-    ...
-    print("Get Picture: Execution completed")
+def getPicture(pictureToGet,containerName):
+	conn = getSwiftConn()
+	picture = conn.get_object(containerName, pictureToGet)
+	return picture
 
 if __name__ == '__main__':
 	app.run(host = "ServerP", port=80)
