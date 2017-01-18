b8ce8bdd5bf778764488d9fa1ce2bdc9b9aeb347
InsafB <boukrouh.insaf@gmail.com>
2017-01-18 22:31:02 +0100

    Update scripts_dependencies.sh

--- AutomatedDeploymentScripts/scripts_dependencies.sh
+++ AutomatedDeploymentScripts/scripts_dependencies.sh
@@ -12,29 +12,13 @@ else
     echo "python3-pip installation failed"
 fi
 
-if sudo apt-get -y install python-openstackclient ; then
-    echo "openstackclient installation succeeded"
-else
-    echo "openstackclient installation failed"
-fi
-
-if sudo apt-get -y install python3-swiftclient ; then
-    echo "swiftclient installation succeeded"
-else
-    echo "swiftclient installation failed"
-fi
-
-if sudo apt-get -y install python-neutronclient ; then
-    echo "neutronclient installation succeeded"
-else
-    echo "neutronclient installation failed"
-fi
-
-if sudo apt-get -y install python-paramiko ; then
-    echo "paramiko installation succeeded"
-else
-    echo "paramiko installation failed"
-fi
+if ("$1" = "ServerB" || "$1" = "ServerP") then
+    if sudo apt-get -y install python3-swiftclient ; then
+        echo "swiftclient installation succeeded"
+    else
+        echo "swiftclient installation failed"
+    fi
+fi   
 
 if sudo pip3 install flask ; then
     echo "flask installation succeeded"
@@ -42,26 +26,32 @@ else
     echo "flask installation failed"
 fi
 
-if sudo pip3 install Flask-Mail ; then
-    echo "flask-mail installation succeeded"
-else
-    echo "flask-mail installation failed"
-fi
+if ("$1" = "ServerM") then
+    if sudo pip3 install Flask-Mail ; then
+        echo "flask-mail installation succeeded"
+    else
+        echo "flask-mail installation failed"
+    fi
+fi    
 
-if sudo pip3 install pymysql ; then
-    echo "pymysql installation succeeded"
-else
-    echo "pymysql installation failed"
-fi
+if ("$1" = "ServerB" || "$1" = "ServerP") then
+    if sudo pip3 install pymysql ; then
+        echo "pymysql installation succeeded"
+    else
+        echo "pymysql installation failed"
+    fi
+fi    
 
-wget http://www.imagemagick.org/download/ImageMagick.tar.gz
-tar -xvf ImageMagick.tar.gz
-cd ImageMagick-7.*
-./configure
-make
-if sudo make install ; then
-    echo "MagikImage installation succeeded"
-    sudo ldconfig /usr/local/lib 
-else
-    echo "MagikImage installation failed"
+if ("$1" = "ServerW") then
+    wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+    tar -xvf ImageMagick.tar.gz
+    cd ImageMagick-7.*
+    ./configure
+    make
+    if sudo make install ; then
+        echo "MagikImage installation succeeded"
+        sudo ldconfig /usr/local/lib 
+    else
+        echo "MagikImage installation failed"
+    fi
 fi
