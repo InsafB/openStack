3860c6242ec85cbf9bb6d2f8984aa907a300cf7f
ouiame-aitelkadi <ouiame.aitelkadi@gmail.com>
2017-01-21 19:24:07 +0100

    Update i.py

--- Services/i/i.py
+++ Services/i/i.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET', 'POST'])
 def identification():
 	# Database config
-	db = MySQLdb.connect(host='ServerI', user="root", passwd="othmane", db="open_stack")
+	db = MySQLdb.connect(host='localhost', user="root", passwd="othmane", db="open_stack")
 	cur = db.cursor()
 	# Get master request with user_id
 	user_id = request.args.get('user_id')
