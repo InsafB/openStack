5e248735170bf79d87b45210ae218d2ce6fff867
InsafB <boukrouh.insaf@gmail.com>
2017-01-21 20:48:40 +0200

    Update sendProject.sh

--- sendProject.sh
+++ sendProject.sh
@@ -6,8 +6,8 @@ rsa_pub=$3
 
 scp -r openStack-master ubuntu@$ip:~/
 
-scp rsa_priv ubuntu@$ip:~/.ssh/
+scp $rsa_priv ubuntu@$ip:~/.ssh/
 
-scp rsa_pub ubuntu@$ip:~/.ssh/
+scp $rsa_pub ubuntu@$ip:~/.ssh/
 
 scp openStack-master/config ubuntu@$ip:~/.ssh/
